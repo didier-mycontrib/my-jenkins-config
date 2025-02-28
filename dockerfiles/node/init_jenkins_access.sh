f4a2b36b37817a70f38337fcb159e1714aae840e
Didier_Defrance <didier@d-defrance.fr>
2025-02-28 12:56:57 +0100

    s

--- dockerfiles/node/init_jenkins_access.sh
+++ dockerfiles/node/init_jenkins_access.sh
@@ -1,6 +1,5 @@
 #after apt-get install -y acl
 setfacl --modify user:jenkins:rw /var/run/docker.sock
 
-#after mkdir /home/jenkins/.npm-global and node/npm installation
-npm config set prefix '/home/jenkins/.npm-global'
-export PATH=/home/jenkins/.npm-global/bin:$PATH
+#source .profile
+. /home/jenkins/.profile
