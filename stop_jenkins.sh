aacf987881c935973821f83df5f1b177703f24ef
Didier_Defrance <didier@d-defrance.fr>
2025-03-04 11:24:22 +0100

    s

--- stop_jenkins.sh
+++ stop_jenkins.sh
@@ -1,3 +1,8 @@
+docker stop desktop-jenkins_agent-1-node
+docker rm desktop-jenkins_agent-1-node
+docker stop my-jenkins-config-jenkins_controller-1
+docker rm my-jenkins-config-jenkins_controller-1
+
 #sudo docker compose --profile python down -v --remove-orphans
 #docker compose --profile node down -v --remove-orphans
 docker compose --profile node down -v 
